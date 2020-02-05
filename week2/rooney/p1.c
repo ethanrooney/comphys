2aba1bc66e350d816ba116de47f42bc68d59d8cc
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-02-04 20:50:35 -0500

    homework 2

--- week2/rooney/p1.c
+++ week2/rooney/p1.c
@@ -15,9 +15,11 @@ double integrateleft(double a, double b, long int N)
 {
 	double delx = (b - a)/N;
 	double res = 0;
-
+	double x = 0;
 	for(int i=0; i<N; i++)
 	{
+		x = delx*i;
+		printf("%e\t%e\n", x, res);
 		res += delx*f(a+delx*i); //you don't need to put in {} if it is a single line statment
 	}
 
@@ -27,9 +29,12 @@ double integratemiddle(double a, double b, long long int N)
 {
 	double delx = (b - a)/N;
 	double res = 0;
+	double x = 0;
 
 	for(long long int i=0; i<N; i++)
 	{
+		x = delx*i;
+		printf("%e\t%e\n", x, res);
 		res += delx*f(a+delx*i+0.5*delx); //you don't need to put in {} if it is a single line statment
 	}
 
@@ -40,9 +45,13 @@ int main(int argv, char** args)
 {
 	if(argv < 4)
 	{
-		double resMiddle = integratemiddle(0, 1, atoi(args[1]));
-		double resLeft = integrateleft(0,1, atoi(args[1]));
-		printf("%15.15lf\t%15.15lf\n", resMiddle, resLeft);
+		if(strncmp(args[1], "-m", 2)){
+			integratemiddle(0, 1, atoi(args[2]));
+		}
+		if(strncmp(args[1], "-l", 2)){
+			integrateleft(0,1, atoi(args[2]));
+		}
+		printf("%c", strncmp(args[1], "-m",2));
 	}
 	else
 	{
