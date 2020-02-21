d973abc7c5452a4d51c9deb62bc27ffdc82cb6d2
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-02-20 21:41:44 -0500

    test

--- week4/coding/nbody.c
+++ week4/coding/nbody.c
@@ -37,32 +37,32 @@ void F( double* y, double t, double* res, double* mass )
 int main(int argc, char** argv)
 {
 	double	dt		= atof(argv[1]);
-	double	tmax	= atoi(argv[2]);
-	double	m0		= atoi(argv[3]);
-	double	x0		= atoi(argv[4]);
-	double	y0		= atoi(argv[5]);
-	double	vx0		= atoi(argv[6]);
-	double	vy0		= atoi(argv[7]);
-	double	m1		= atoi(argv[8]);
-	double	x1		= atoi(argv[9]);
-	double	y1		= atoi(argv[10]);
-	double	vx1		= atoi(argv[11]);
-	double	vy1		= atoi(argv[12]);
-	double	m2		= atoi(argv[13]);
-	double	x2		= atoi(argv[14]);
-	double	y2		= atoi(argv[15]);
-	double	vx2		= atoi(argv[16]);
-	double	vy2		= atoi(argv[17]);
+	double	tmax	= atof(argv[2]);
+	double	m0		= atof(argv[3]);
+	double	x0		= atof(argv[4]);
+	double	y0		= atof(argv[5]);
+	double	vx0		= atof(argv[6]);
+	double	vy0		= atof(argv[7]);
+	double	m1		= atof(argv[8]);
+	double	x1		= atof(argv[9]);
+	double	y1		= atof(argv[10]);
+	double	vx1		= atof(argv[11]);
+	double	vy1		= atof(argv[12]);
+	double	m2		= atof(argv[13]);
+	double	x2		= atof(argv[14]);
+	double	y2		= atof(argv[15]);
+	double	vx2		= atof(argv[16]);
+	double	vy2		= atof(argv[17]);
 
 	double mass[NBODY] = {m0,m1,m2};
 
-	double y[4*NBODY] = {x0,y0,x1,y1,x2,y2,vx0,vy0,vx1,vy1,vx2, vy2}, yn[4*NBODY], t=0;
+	double y[4*NBODY] = {x0,y0,x1,y1,x2,y2,vx0,vy0,vx1,vy1,vx2,vy2}, yn[4*NBODY], t=0;
 
 	for (int i=0; i*dt<tmax-0.5*dt; ++i)
 	{
 		integrate_rk2(y,4*NBODY, t, dt, yn, mass, F);
 		printf("%4.4f ", t+dt);
-		for ( int k=0; k<4*NBODY; k++) printf("%5.5f ", yn[k]);
+		for ( int k=0; k<4*NBODY; k++) printf("%15.15f ", yn[k]);
 		printf("\n");
 		for(int k=0; k<4*NBODY; ++k) y[k] = yn[k];
 		t = t + dt;
