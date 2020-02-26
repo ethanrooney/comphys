478188be4cae613b8e6bbf4b9afa013ea795bcbd
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-02-26 14:43:28 -0500

    Quiz

---
+++ week5/quiz/quiz.c
@@ -0,0 +1,37 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  quiz.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/26/2020 02:30:54 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Ethan Rooney (), ethan.f.rooney@protonmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include	<stdlib.h>
+#include	<stdio.h>
+#include	<math.h>
+
+double lognfact(int n)
+{
+	double res =0;
+	for(int i = 1; i<=n; i++)
+	{
+		res = res+log(i);
+	}
+	return res;
+}
+	
+
+int main(int argc, char** argv)
+{
+	printf("%lf\n",lognfact(atoi(argv[1])));
+}
+
