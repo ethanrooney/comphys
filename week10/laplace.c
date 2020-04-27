dce41954da178aaba19eb505c7554231ba9c82af
Ethan Rooney <ethan.f.rooney@protonmail.com>
2020-04-27 15:42:39 -0400

    week 10 code

--- week10/laplace.c
+++ week10/laplace.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-type struct
+typedef struct
 {
   int index;
   double x,y;
@@ -27,6 +28,29 @@ void setup(int N, int *nphi, int *ne, double *pos)
     p[idx].ne[3] = (i-1)*N+j;
   }
 
+  double eps = 1e-10;
+  for(int i=0; i<N*N; ++i)
+  {
+    if(fabs(p[i].x)>1.5-eps) p[i].index = -1;
+    if(fabs(p[i].y)>1.5-eps) p[i].index = -1;
+    if((fabs(p[i].x)<0.5+eps) && (fabs(p[i].y)<0.5+eps)) p[i].index=-2;
+  }
+
+  int newidx=0;
+  for(int i=0; i<N*N; ++i) if(p[i].index >= 0) p[i].index=newidx++;
+  *nphi = newidx;
+
+  for(int i=0; i<N*N; ++i) if(p[i].index >= 0)
+  {
+    int idx = p[i].index;
+    pos[2*idx+0] = p[i].x;
+    pos[2*idx+1] = p[i].y;
+    ne[4*idx+0] = p[p[i].ne[0]].index;
+    ne[4*idx+1] = p[p[i].ne[1]].index;
+    ne[4*idx+2] = p[p[i].ne[2]].index;
+    ne[4*idx+3] = p[p[i].ne[3]].index;
+  }
+
   free(p);
 }
 
@@ -41,7 +65,9 @@ void gauss_seidel(int nphi, double* phi, int *ne, double delta)
               + phi[ ne[4*i+3] ])/4;
       err += (tmp - phi[i])*(tmp-phi[i]);
       nrm += tmp*tmp;
+      phi[i] = tmp;
     }
+    printf("err: %e nrm: %e\n", sqrt(err), sqrt(nrm));
     if( sqrt(err/nrm) < delta) break;
   }
 }
@@ -57,12 +83,33 @@ int main(int argc, char** argv)
   int nphi;
   setup(N, &nphi, ne, pos);
 
+  printf("nphi: %d\n", nphi);
+  for(int i=0; i<nphi; ++i) 
+    printf("ne[%d]: %d %d %d %d\n", i, ne[4*i+0], ne[4*i+1], 
+      ne[4*i+2], ne[4*i+3]);
+
   double* phi = malloc((nphi+2)*sizeof(double));
   phi[0] = 0;
   phi[1] = 100;
+  for(int i=0; i<nphi; ++i) phi[2+i] = 0;
 
   gauss_seidel(nphi, phi+2, ne, delta);
 
+  for(int i=0; i<nphi; ++i)
+    printf("phi: % +10e  % +10e  % +20e\n", pos[2*i+0], pos[2*i+1],
+      phi[2+i]);
+
+  double a = 3.0/(N-1);
+  for(int i=0; i<nphi; ++i)
+  {
+    double ex = -(phi[2+ne[4*i+0]] - phi[2+ne[4*i+1]])/(2*a);
+    double ey = -(phi[2+ne[4*i+2]] - phi[2+ne[4*i+3]])/(2*a);
+    printf("efield: %+10e %+10e  %20e %20e\n", pos[2*i+0], pos[2*i+1],
+      ex, ey);
+  }
+
   free(ne);
+  free(pos);
+  free(phi); 
   return 0;
 }
